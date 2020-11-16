3eee567cdb1f681a582eb4234e103d5ae6cddb68
strawnp <42393469+strawnp@users.noreply.github.com>
2020-11-16 10:34:08 -0800

    Update Console.java

--- Console.java
+++ Console.java
@@ -1,3 +1,25 @@
 public class Console {
+    // instance variables --> private
+    // keyword for constant --> final
+    // private String name;
+    // private final int numHDMIOutputs = 3;
 
+    // class variables
+    // keyword --> static
+
+    // job of constructors --> initialize variables
+    // default, no-argument constructor
+
+    // parameterized constructor
+
+    // methods
+    // getters --> access (instance) variables
+
+    // setters --> mutate (instance) variables
+
+    // toString() --> what displays when println() is called
+
+    // equals() --> check for object equality
+
+    // any other helper methods (public or private)
 }
