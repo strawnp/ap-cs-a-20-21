877da26b366236f1bbbdfe31141b550a307ab708
strawnp <42393469+strawnp@users.noreply.github.com>
2021-01-14 08:58:24 -0800

    Add files via upload

---
+++ ArrayAlgorithms.java
@@ -0,0 +1,26 @@
+public class ArrayAlgorithms {
+
+    /**
+     *
+     * INSTRUCTIONS
+     * You are creating algorithms that are important to understand
+     * in the context of arrays. To begin, you will be generating
+     * randomized lists, one as an array and one as an ArrayList.
+     * From there, you will be developing a series of methods that
+     * you will call for each of the data structures you have
+     * created. In total you will be writing 6 methods since there
+     * are 3 calculations and 2 data structures to perform them on.
+     *
+    **/
+    public static void main(String[] args) {
+        // create array of 50 randomized integers between 1 and 1000
+
+        // create ArrayList of 50 randomized integers between 1 and 1000
+
+        // find and print the mean value of each list
+
+        // find and print the median value of each list
+
+        // find and print the minimum and maximum values of each list 
+    }
+}
